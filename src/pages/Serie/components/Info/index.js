0e9fe8ba8ae8d503905fc90cd06a45388ecd592e
eliezergarcia <nhernandez@lap55.com>
2021-02-11 11:56:49 -0600

    edit - se cambia información de contacto en VOD (se quita título y descripción)

--- src/pages/Serie/components/Info/index.js
+++ src/pages/Serie/components/Info/index.js
@@ -17,43 +17,36 @@ export function Info({ data }) {
 
       return (
             <div className="info-contact-wrapper">
+                  
                   <div className="group-section">
-                        {ContactTitle &&
-                              <div className="title-group">{ContactTitle}</div>
-                        }
-                        {ContactDescription &&
-                              <div className="description-group">{ContactDescription}</div>
-                        }
-                  </div>
-                  <div className="group-section">
-                        <div className="phone-group"><i className="fas fa-phone-alt"></i>{ContactFon}</div>
-                        <div className="web-group"><i className="fas fa-globe"></i><a href={ContactWeb}>{ContactWeb}</a></div>
+                        <div className="phone-group"><i className="fas fa-phone-alt" />Télefono de contacto: &nbsp;&nbsp;&nbsp;{ContactFon}</div>
+                        <div className="web-group"><i className="fas fa-globe" />Sitio web: &nbsp;&nbsp;&nbsp;<a href={ContactWeb}>{ContactWeb}</a></div>
                         <div className="social-media-group">
                               {ContactFb &&
                                     <Tooltip title="Facebook" placement="top-start">
                                           <a href={`https://www.facebook.com/${ContactFb}`}>
-                                                <i className="fab fa-facebook-square" />
+                                                <i className="fab fa-facebook-square" /> Facebook
                                           </a>
                                     </Tooltip>
                               }
                               {ContactIG &&
                                     <Tooltip title="Instagram" placement="top-start">
                                           <a href={`https://www.instagram.com/${ContactIG}`}>
-                                                <i className="fab fa-instagram" />
+                                                <i className="fab fa-instagram" /> Instagram
                                           </a>
                                     </Tooltip>
                               }
                               {ContactTw &&
                                     <Tooltip title="Twitter" placement="top-start">
                                           <a href={`https://www.twitter.com/${ContactTw}`}>
-                                                <i className="fab fa-twitter-square" />
+                                                <i className="fab fa-twitter-square" /> Twitter
                                           </a>
                                     </Tooltip>
                               }
                               {ContactLoc &&
                                     <Tooltip title="Ubicación" placement="top-start">
                                           <a href={`https://www.google.com/maps/place/${replaceString(ContactLoc, ",", "+")}`}>
-                                                <i className="fas fa-map-marker-alt" />
+                                                <i className="fas fa-map-marker-alt" /> Ubicación
                                           </a>
                                     </Tooltip>
                               }
