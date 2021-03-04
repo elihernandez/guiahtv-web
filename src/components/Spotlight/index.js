263061df5b8dfa555cc26a26788e06e1323e22f8
eliezergarcia <nhernandez@lap55.com>
2021-03-04 16:21:15 -0600

    add - se agrega componente ReactSlick para Slider de Spotlight

--- src/components/Spotlight/index.js
+++ src/components/Spotlight/index.js
@@ -1,22 +1,67 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getSpotlight } from '../../services/getSpotlight'
-import { Slider, SliderContent, SliderIndicators } from '../Slider'
-import { CSSTransition } from 'react-transition-group'
+// import { Slider, SliderContent, SliderIndicators } from '../Slider'
+// import { CSSTransition } from 'react-transition-group'
+import { LazyImage } from '../Image'
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 import './styles.css'
 
-export function Spotlight(){
+function SamplePrevArrow(props) {
+    const { className, onClick } = props
+
+    return (
+        <div
+            className={className}
+            onClick={onClick}
+        >
+            <button><i className="fas fa-angle-left"></i></button>
+        </div>
+    )
+}
+
+function SampleNextArrow(props) {
+    const { className, onClick } = props
+
+    return (
+        <div
+            className={className}
+            onClick={onClick}
+        >
+            <button><i className="fas fa-angle-right"></i></button>
+        </div>
+    )
+}
+
+
+export function Spotlight() {
     const [loading, setLoading] = useState(true)
     const [data, setData] = useState(null)
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: false,
+        pauseOnHover: true,
+        prevArrow: <SamplePrevArrow />,
+        nextArrow: <SampleNextArrow />
+    }
+
     useEffect(() => {
         const requestSpotlight = async () => {
-            try{
+            try {
                 setLoading(true)
                 const response = await getSpotlight()
-                if(!response.length) throw new Error('No se pudo obtener la información.')
+                if (!response.length) throw new Error('No se pudo obtener la información.')
                 setData(response)
                 setLoading(false)
-            }catch(e){
+            } catch (e) {
                 console.log(e)
             }
         }
@@ -26,16 +71,17 @@ export function Spotlight(){
 
     return (
         <div className="spotlight-wrapper">
-            <CSSTransition in={!loading} timeout={100} classNames="fade">
-                <div className="slider-spotlight">
-                    {data &&
-                        <Slider controls={true} mode="center">
-                            <SliderContent data={data}/>
-                            <SliderIndicators data={data}/>
-                        </Slider>
+            {data &&
+                <Slider {...settings}>
+                    {
+                        data.map(({ Registro, ImgLandscape }, index) =>
+                            <div key={Registro} style={{ width: '100%' }}>
+                                <LazyImage img={ImgLandscape} alt={`spotlight-image-${index}`} type="webp" recoverType="png" />
+                            </div>
+                        )
                     }
-                </div>
-            </CSSTransition>
+                </Slider>
+            }
         </div>
     )
 }
