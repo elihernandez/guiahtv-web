82068723544864a0e634250c8b54e175efe3a2db
eliezergarcia <nhernandez@lap55.com>
2021-04-02 12:45:24 -0600

    edit - se corrige error de PopperMenu en temporadas de serie, el menú se ocultaba al hacer scroll

--- src/components/PopperMenu/index.js
+++ src/components/PopperMenu/index.js
@@ -9,74 +9,74 @@ import MenuList from '@material-ui/core/MenuList'
 import './styles.css'
 
 export function PopperMenu({textButton, itemsMenu}){
-      const anchorRef = useRef(null)
-      const [open, setOpen] = useState(false)
+	const anchorRef = useRef(null)
+	const [open, setOpen] = useState(false)
     
-      const handleToggle = () => {
-        setOpen((prevOpen) => !prevOpen)
-      }
+	const handleToggle = () => {
+		setOpen((prevOpen) => !prevOpen)
+	}
     
-      const handleClose = (event) => {
-        if (anchorRef.current && anchorRef.current.contains(event.target)) {
-          return
-        }
+	const handleClose = (event) => {
+		if (anchorRef.current && anchorRef.current.contains(event.target)) {
+			return
+		}
     
-        setOpen(false)
-      }
+		setOpen(false)
+	}
     
-      function handleListKeyDown(event) {
-        if (event.key === 'Tab') {
-          event.preventDefault()
-          setOpen(false)
-        }
-      }
+	function handleListKeyDown(event) {
+		if (event.key === 'Tab') {
+			event.preventDefault()
+			setOpen(false)
+		}
+	}
     
-      // return focus to the button when we transitioned from !open -> open
-      const prevOpen = useRef(open)
-      useEffect(() => {
-        if (prevOpen.current === true && open === false) {
-          anchorRef.current.focus()
-        }
+	// return focus to the button when we transitioned from !open -> open
+	const prevOpen = useRef(open)
+	useEffect(() => {
+		if (prevOpen.current === true && open === false) {
+			anchorRef.current.focus()
+		}
     
-        prevOpen.current = open
-      }, [open])
+		prevOpen.current = open
+	}, [open])
     
-      return (
-        <div className="popper-menu-wrapper">
-          <Button
-            ref={anchorRef}
-            aria-controls={open ? 'menu-list-grow' : undefined}
-            aria-haspopup="true"
-            onClick={handleToggle}
-            >
-            {textButton}
-          </Button>
-          <Popper open={open} anchorEl={anchorRef.current} placement="top" role={undefined} transition disablePortal>
-            {({ TransitionProps, placement }) => (
+	return (
+		<div className="popper-menu-wrapper">
+			<Button
+				ref={anchorRef}
+				aria-controls={open ? 'menu-list-grow' : undefined}
+				aria-haspopup="true"
+				onClick={handleToggle}
+			>
+				{textButton}
+			</Button>
+			<Popper open={open} anchorEl={anchorRef.current} placement="bottom" role={undefined} transition disablePortal >
+				{({ TransitionProps, placement }) => (
              
-                <Paper>
-                  <ClickAwayListener onClickAway={handleClose}>
-                    <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
-                      {
-                        itemsMenu.map(({title, href, func}) => {
-                          if(func != ''){
-                            return <a key={title} href={href} target="_blank">
-                              <MenuItem onClick={func}>{title}</MenuItem>
-                            </a>
-                          }else{
-                            return <a key={title} href={href} target="_blank">
-                              <MenuItem className="body-3">{title}</MenuItem>
-                            </a>
-                          }
-                        })
-                      }
-                    </MenuList>
-                  </ClickAwayListener>
-                </Paper>
+					<Paper>
+						<ClickAwayListener onClickAway={handleClose}>
+							<MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+								{
+									itemsMenu.map(({title, href, func}) => {
+										if(func != ''){
+											return <a key={title} href={href} target="_blank">
+												<MenuItem onClick={func}>{title}</MenuItem>
+											</a>
+										}else{
+											return <a key={title} href={href} target="_blank">
+												<MenuItem className="body-3">{title}</MenuItem>
+											</a>
+										}
+									})
+								}
+							</MenuList>
+						</ClickAwayListener>
+					</Paper>
               
-            )}
-          </Popper>
-        </div>
-      )
+				)}
+			</Popper>
+		</div>
+	)
 }
     
