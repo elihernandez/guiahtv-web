2cf54f44a531d195ab979c36be4016f77d7d53bb
eliezergarcia <nhernandez@lap55.com>
2021-04-15 14:56:46 -0500

    edit - Video, se agrega condición para detectar si hay un error 403 y hacer hls.startLoad() cada segundo hasta que el servidor responda nuevamente y el video no se pare.

--- src/hooks/useHls.js
+++ src/hooks/useHls.js
@@ -1,28 +1,28 @@
 import { useState, useEffect } from 'react'
 import Hls from 'hls.js'
 // console.log(Hls.version)
+const config = {
+	debug: process.env.NODE_ENV !== 'production' ? false : false,
+	initialLiveManifestSize: 3,
+	liveBackBufferLength: 900,
+	enableWorker: false,
+	nudgeMaxRetry: 10,
+	manifestLoadingMaxRetry: 10,
+	fragLoadingRetryDelay: 3000,
+	manifestLoadingRetryDelay: 3000,
+	levelLoadingRetryDelay: 3000,
+}
 
 export function useHls(video, url, dispatch, movie) {
-	const config = {
-		debug: process.env.NODE_ENV !== 'production' ? false : false,
-		initialLiveManifestSize: 3,
-		liveBackBufferLength: 900,
-		enableWorker: false,
-		nudgeMaxRetry: 10,
-		manifestLoadingMaxRetry: 10,
-		fragLoadingRetryDelay: 3000,
-		manifestLoadingRetryDelay: 3000,
-		levelLoadingRetryDelay: 3000,
-	}
-	let hls = new Hls(config)
+	const hls = new Hls(config)
 	const [error, setError] = useState(false)
-	const [recover, setRecover] = useState(0)
 
 	useEffect(() => {
 		if(url){
+			setError(false)
 			if (Hls.isSupported()) {
-				setError(false)
 				hls.attachMedia(video.current)
+
 				hls.on(Hls.Events.MEDIA_ATTACHED, function () {
 					hls.loadSource(url)
 					hls.on(Hls.Events.MANIFEST_PARSED, function () {
@@ -36,13 +36,15 @@ export function useHls(video, url, dispatch, movie) {
 						dispatch({ type: 'setHls', payload: hls })
 					})
 				})
+
+				hls.on(Hls.Events.MEDIA_DETACHING, function(){
+					video.current.pause()
+				})
 			
 				hls.on(Hls.Events.ERROR, function (event, data) {
 					if(process.env.NODE_ENV !== 'production'){
 						console.log(event, data)
-					}
-
-					dispatch({ type: 'updateLoading', payload: false })
+					}	
 								
 					if(event == 'hlsError' && data.details == 'manifestLoadError'){
 						hls.destroy()
@@ -53,25 +55,21 @@ export function useHls(video, url, dispatch, movie) {
 					}
 
 					if(data.details == 'bufferStalledError'){
-						// hls.destroy()
 						hls.startLoad()
 						dispatch({ type: 'updateLoading', payload: true })
-						// // dispatch({ type: 'updateData', payload: null })
-						// dispatch({ type: 'setHls', payload: null })
-						// setError("Contenido no disponible por el momento")
 					}
 
 					if(data.details == 'audioTrackLoadError'){
-						if(recover != 3){
-							// hls.destroy()
-							setRecover(recover + 1)
-						}
-						hls.startLoad()
 						hls.recoverMediaError()
+						hls.startLoad()
 						dispatch({ type: 'updateLoading', payload: true })
-						// // dispatch({ type: 'updateData', payload: null })
-						// dispatch({ type: 'setHls', payload: null })
-						// setError("Contenido no disponible por el momento")
+					}
+
+					if(data.response && data.response.code == 403){
+						setTimeout(() => {
+							hls.startLoad()
+						}, 1000)
+						dispatch({ type: 'updateLoading', payload: true })
 					}
 					// if (data.fatal) {
 					//       switch (data.type) {
@@ -86,10 +84,6 @@ export function useHls(video, url, dispatch, movie) {
 				hls.on(Hls.Events.AUDIO_TRACKS_UPDATED, function(event, data){
 					dispatch({ type: 'setAudioTracks', payload: data })
 				})
-
-				hls.on(Hls.Events.MEDIA_DETACHING, function(){
-					video.current.pause()
-				})
 						
 				hls.on(Hls.Events.SUBTITLE_TRACKS_UPDATED, function(event, data){
 					dispatch({ type: 'setSubtitleTracks', payload: data })
@@ -101,80 +95,7 @@ export function useHls(video, url, dispatch, movie) {
 			hls.detachMedia()
 			hls.destroy()
 		})
-	}, [url, recover])
+	}, [url])
 
 	return { error, setError }
 }
-
-// export function useHls(videoRef, url) {
-// 	const video = videoRef.current
-// 	const [success, setSuccess] = useState(false)
-// 	const [error, setError] = useState(false)
-// 	const [audioTracks, setAudioTracks] = useState([])
-// 	const [subtitleTracks, setSubtitleTracks] = useState([])
-
-// 	const config = {
-// 		debug: process.env.NODE_ENV !== 'production' ? false : false,
-// 		initialLiveManifestSize: 3,
-// 		liveBackBufferLength: 900,
-// 		enableWorker: true,
-// 		nudgeMaxRetry: 10,
-// 		manifestLoadingMaxRetry: 10,
-// 		fragLoadingRetryDelay: 3000,
-// 		manifestLoadingRetryDelay: 3000,
-// 		levelLoadingRetryDelay: 3000,
-// 	}
-
-// 	const hls = new Hls(config)
-
-// 	useEffect(() => {
-// 		if(url){
-// 			setSuccess(false)
-// 			setError(false)
-// 			if (Hls.isSupported()) {
-// 				hls.attachMedia(video)
-
-// 				hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-// 					hls.loadSource(url)
-// 					hls.on(Hls.Events.MANIFEST_PARSED, function () {
-// 						setSuccess(true)
-// 					})
-// 				})
-            
-// 				hls.on(Hls.Events.ERROR, function (event, data) {
-// 					if(process.env.NODE_ENV !== 'production'){
-// 						console.log(event, data)
-// 					}
-                              
-// 					if(event == 'hlsError' && data.details == 'manifestLoadError'){
-// 						hls.destroy()
-// 						setError('Señal no disponible por el momento')
-// 					}
-
-// 					if(data.details == 'bufferStalledError'){
-// 						hls.startLoad()
-// 					}
-
-// 					if(data.details == 'audioTrackLoadError'){
-// 						hls.startLoad()
-// 						hls.recoverMediaError()
-// 					}
-// 				})
-
-// 				hls.on(Hls.Events.AUDIO_TRACKS_UPDATED, function(event, data){
-// 					setAudioTracks(data)
-// 				})
-				
-// 				hls.on(Hls.Events.SUBTITLE_TRACKS_UPDATED, function(event, data){
-// 					setSubtitleTracks(data)
-// 				})
-// 			}
-// 		}
-
-// 		return (() => {
-// 			hls.destroy()
-// 		})
-// 	}, [url])
-
-// 	return { success, error, audioTracks, subtitleTracks }
-// }
